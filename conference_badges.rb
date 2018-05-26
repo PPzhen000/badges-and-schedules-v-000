9034c23be2b2126e2127ffff3c2c19d097f8baa1
Ariel Zhen <zhenpingping@live.com>
2018-05-26 06:59:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
-array_of_speakers = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+array_of_speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
   return "Hello, my name is #{name}."
