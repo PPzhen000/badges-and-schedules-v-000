b983708395f170ae5148ab196fb961f94f0b5684
Ariel Zhen <zhenpingping@live.com>
2018-05-26 06:50:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+# array_of_speakers = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+# def batch_badge_creator(array_of_speakers)
+#   result = []
+#   array_of_speakers.each {|x| result << badge_maker(x)}
+#   return result
+# end
+#
+# def assign_rooms(array_of_speakers)
+#   result = []
+#   array_of_speakers.each_with_index{|x, index| result <<  "Hello, #{x}! You'll be assigned to room #{index+1}!"}
+# end
+#
+# def printer
+#   puts batch_badge_creator(array_of_speakers)
+#   puts assign_rooms(array_of_speakers)
+# end
